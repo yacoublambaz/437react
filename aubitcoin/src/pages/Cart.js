a833526b5a375f58dfa3ab8cd169f9e69683ebf5
alimans587 <alimans587@gmail.com>
2022-04-08 19:10:03 +0300

    working on ui (carousel...)

--- aubitcoin/src/pages/Cart.js
+++ aubitcoin/src/pages/Cart.js
@@ -3,8 +3,11 @@ import React, { useState } from "react";
 import { getUserToken, saveUserToken, clearUserToken } from "../localStorage";
 import HomeIcon from "@mui/icons-material/Home";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import ChildCareIcon from '@mui/icons-material/ChildCare';
 import SearchIcon from "@mui/icons-material/Search";
 import LoginIcon from "@mui/icons-material/Login";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import {
   IconButton,
@@ -26,57 +29,86 @@ import BoughtTransaction from "../transaction/boughtTransaction";
 import SoldTransaction from "../transaction/soldTransaction";
 
 function Cart(){
+
+    const States = {
+        PENDING: "PENDING",
+        USER_CREATION: "USER_CREATION",
+        USER_LOG_IN: "USER_LOG_IN",
+        USER_AUTHENTICATED: "USER_AUTHENTICATED",
+        HOME: "HOME",
+      };
+
+    let [userToken, setUserToken] = useState(getUserToken());
+    let [authState, setAuthState] = useState(States.PENDING);
+
     const navigate = useNavigate();
 
-    function Gotomain(){
-        navigate('/', {replace:true});
+    function Gotoprofile(){
+        navigate('/profile', {replace:true});
     }
 
     function Gotohome(){
-        navigate('/home', {replace:true})
+        navigate('/', {replace:true})
     }
 
     function Gotocart(){
         navigate('/cart', {replace:true});
     }
 
+    function Gotosearch(){
+        navigate('/search', {replace:true});
+    }
+
+    function logout() {
+        setUserToken(null);
+        clearUserToken();
+      }
+
     return(
-        <><div style={{color:"white"}}>Welcome to Cart</div>
+        <div className="App">
             <Box display="flex" flexDirection="row">
                 <Box className="navbar">
-                    <List className="navbar-li">
+                        <List className="navbar-li">
                         <ListItem>
-                            <IconButton>
-                                <Icon style={{ width: 40, height: 40 }}>
-                                    <img className="logoIm" src="./abc.png" />
-                                </Icon>
+                        <IconButton>
+                            <Icon style={{ width: 40, height: 40 }}>
+                                <img className="logoIm" src="./abc.png" />
+                            </Icon>
+                        </IconButton>
+                        </ListItem>
+                        <ListItem>
+                            <IconButton onClick={Gotoprofile}>
+                                <ChildCareIcon
+                                className="navbar-icon"
+                                sx={{ color: "#f3c317", fontSize: 40 }}
+                                />
                             </IconButton>
                         </ListItem>
                         <ListItem>
                             <IconButton onClick={Gotohome}>
-                                <HomeIcon
+                                <HomeOutlinedIcon
                                     className="navbar-icon"
-                                    sx={{ color: "white", fontSize: 40 }} />
+                                    sx={{ color: "#f3c317", fontSize: 40 }} />
                             </IconButton>
                         </ListItem>
                         <ListItem>
                             <IconButton onClick={Gotocart}>
-                                <ShoppingCartIcon
+                                <ShoppingCartOutlinedIcon
                                     className="navbar-icon"
                                     sx={{ color: "white", fontSize: 40 }} />
                             </IconButton>
                         </ListItem>
                         <ListItem>
-                            <IconButton>
+                            <IconButton onClick={Gotosearch}>
                                 <SearchIcon
                                     className="navbar-icon"
-                                    sx={{ color: "white", fontSize: 40 }} />
+                                    sx={{ color: "#f3c317", fontSize: 40 }} />
                             </IconButton>
                         </ListItem>
                     </List>
                 </Box>
             </Box>
-        </>
+        </div>
     )
 }
 export default Cart;
--- aubitcoin/src/pages/Profile.js
+++ aubitcoin/src/pages/Profile.js
@@ -2,10 +2,13 @@ import "../App.css";
 import React, { useState } from "react";
 import { getUserToken, saveUserToken, clearUserToken } from "../localStorage";
 import HomeIcon from "@mui/icons-material/Home";
+import ChildCareIcon from '@mui/icons-material/ChildCare';
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import SearchIcon from "@mui/icons-material/Search";
 import LoginIcon from "@mui/icons-material/Login";
-import { BrowserRouter, Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { BrowserRouter, Navigate, Route, Routes, useNavigate, Redirect } from "react-router-dom";
 import {
   IconButton,
   Typography,
@@ -24,162 +27,119 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import { ConstraintLayout, ConstraintGuide } from "react-constraint-layout";
 import BoughtTransaction from "../transaction/boughtTransaction";
 import SoldTransaction from "../transaction/soldTransaction";
-import Cart from "./Cart"
-import Home from "./Home"
+import Cart from "./Cart";
+import Home from "./Home";
 
-var SERVER_URL = "http://127.0.0.1:5000";
+import { SERVER_URL } from "../App"
 
 function Profile() {
-  const Pages = {
-    PROFILE: "PROFILE",
-    HOME: "HOME",
-    CART: "CART",
-    SEARCH: "SEARCH",
-  };
 
-  const States = {
-    PENDING: "PENDING",
-    USER_CREATION: "USER_CREATION",
-    USER_LOG_IN: "USER_LOG_IN",
-    USER_AUTHENTICATED: "USER_AUTHENTICATED",
-    HOME: "HOME",
-  };
+    const States = {
+        PENDING: "PENDING",
+        USER_CREATION: "USER_CREATION",
+        USER_LOG_IN: "USER_LOG_IN",
+        USER_AUTHENTICATED: "USER_AUTHENTICATED",
+        HOME: "HOME",
+      };
 
-  let [userToken, setUserToken] = useState(getUserToken());
-  let [authState, setAuthState] = useState(States.PENDING);
-  let [pageState, setpageState] = useState(Pages.Home);
-  let [userId, setUserId] = useState(null);
-  let [userName, setUserName] = useState(null);
-  let [userBalance, setUserBalance] = useState(0);
-  let [userInitial, setUserInitial] = useState(null);
+    let [userToken, setUserToken] = useState(getUserToken());
+    let [authState, setAuthState] = useState(States.PENDING);
+    let [userId, setUserId] = useState(null);
+    let [userName, setUserName] = useState(null);
+    let [userBalance, setUserBalance] = useState(0);
+    let [userInitial, setUserInitial] = useState(null);
 
-  const navigate = useNavigate();
+    function get_credentials(){
+        if (userToken){
+            return fetch(`${SERVER_URL}/credentials`, {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body:JSON.stringify({
+                    token: userToken
+                }),
+            })
+                .then((response) => response.json())
+                .then((body) => {
+                setUserId(body.user_id);
+                setUserName(body.user_name);
+                setUserInitial(body.user_initial)
+                setUserBalance(body.balance);
+                });
+        }
+    }
 
-  function Gotomain(){
-    navigate('/', {replace:true});
-  }
+    const navigate = useNavigate();
 
-  function Gotohome(){
-    navigate('/home', {replace:true})
-  }
-
-  function Gotocart(){
-    navigate('/cart', {replace:true});
-  }
-
-  function login(user_id, password) {
-    return fetch(`${SERVER_URL}/authentication`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user_id: user_id,
-        password: password,
-      }),
-    })
-      .then((response) => response.json())
-      .then((body) => {
-        setAuthState(States.USER_AUTHENTICATED);
-        setUserToken(body.token);
-        saveUserToken(body.token);
-        setUserId(user_id);
-        setUserName(body.user_name);
-        setUserInitial(body.user_initial)
-        setUserBalance(body.balance);
-      });
-  }
-
-  function createUser(user_id, username, password, aubEmail) {
-    return fetch(`${SERVER_URL}/signUp`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user_id: user_id,
-        user_name: username,
-        password: password,
-        email: aubEmail,
-      }),
-    }).then((response) => login(user_id, password));
-  }
-
-  function logout() {
-    setUserToken(null);
-    clearUserToken();
-  }
+    function Gotoprofile(){
+        navigate('/profile', {replace:true});
+    }
   
+    function Gotohome(){
+        navigate('/', {replace:true})
+    }
+  
+    function Gotocart(){
+        navigate('/cart', {replace:true});
+    }
+
+    function Gotosearch(){
+        navigate('/search', {replace:true});
+    }
+
+    function logout() {
+        setUserToken(null);
+        clearUserToken();
+        Gotohome();
+      }
+
+    
+    get_credentials();
 
   return (
     <div className="App">
-      <LoginDialog
-        open={States.USER_LOG_IN === authState}
-        title={"LOGIN"}
-        onClose={() => setAuthState(States.PENDING)}
-        submitText={"Login"}
-        onCreateNew={() => setAuthState(States.USER_CREATION)}
-        onSubmit={login}
-      />
-
-      <RegisterDialog
-        open={States.USER_CREATION === authState}
-        title={"REGISTER"}
-        onClose={() => setAuthState(States.PENDING)}
-        submitText={"Register"}
-        onSubmit={createUser}
-      />
-
       <Box display="flex" flexDirection="row">
         <Box className="navbar">
           <List className="navbar-li">
-            <ListItem>
-              <IconButton onClick={Gotomain}>
-                <Icon style={{ width: 40, height: 40 }}>
-                  <img className="logoIm" src="./abc.png" />
-                </Icon>
-              </IconButton>
-            </ListItem>
+                <ListItem>
+                    <IconButton>
+                        <Icon style={{ width: 40, height: 40 }}>
+                            <img className="logoIm" src="./abc.png" />
+                        </Icon>
+                    </IconButton>
+                </ListItem>
+                <ListItem>
+                    <IconButton onClick={Gotoprofile}>
+                        <ChildCareIcon
+                        className="navbar-icon"
+                        sx={{ color: "white", fontSize: 40 }}
+                        />
+                    </IconButton>
+                </ListItem>
             <ListItem>
               <IconButton onClick={Gotohome}>
-                <HomeIcon
+                <HomeOutlinedIcon
                   className="navbar-icon"
-                  sx={{ color: "white", fontSize: 40 }}
+                  sx={{ color: "#f3c317", fontSize: 40 }}
                 />
               </IconButton>
             </ListItem>
             <ListItem>
               <IconButton onClick={Gotocart}>
-                <ShoppingCartIcon
+                <ShoppingCartOutlinedIcon
                   className="navbar-icon"
-                  sx={{ color: "white", fontSize: 40 }}
+                  sx={{ color: "#f3c317", fontSize: 40 }}
                 />
               </IconButton>
             </ListItem>
             <ListItem>
-              <IconButton>
+              <IconButton onClick={Gotosearch}>
                 <SearchIcon
                   className="navbar-icon"
-                  sx={{ color: "white", fontSize: 40 }}
+                  sx={{ color: "#f3c317", fontSize: 40 }}
                 />
               </IconButton>
-            </ListItem>
-            <ListItem className="last">
-              {userToken !== null ? (
-                <IconButton onClick={logout}>
-                  <LogoutIcon
-                    className="navbar-icon"
-                    sx={{ color: "white", fontSize: 40 }}
-                  />
-                </IconButton>
-              ) : (
-                <IconButton onClick={() => setAuthState(States.USER_LOG_IN)}>
-                  <LoginIcon
-                    className="navbar-icon"
-                    sx={{ color: "white", fontSize: 40 }}
-                  />
-                </IconButton>
-              )}
             </ListItem>
           </List>
         </Box>
